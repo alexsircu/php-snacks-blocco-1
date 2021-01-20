624711bda002276463d807ac61b1698b175120c4
Alex Sircu <alex.sricu@gmail.com>
2021-01-20 17:24:58 +0100

    Snack1 completo

--- snack1/index.php
+++ snack1/index.php
@@ -12,35 +12,31 @@ schema:
 Olimpia Milano - Cantù | 55 - 60
 */
 $matches = [
-  "firstMatch" => [
+  "1" => [
     "home_team" => "Detroit Pistons",
     "away_team" => "Boston Cetics",
-    "home_points" => 117,
-    "away_points" => 113
+    "home_points" => "117",
+    "away_points" => "113"
   ],
-  "secondMatch" => [
+  "2" => [
     "home_team" => "L.A. Lakers",
     "away_team" => "G.S. Warriors",
-    "home_points" => 124,
-    "away_points" => 119
+    "home_points" => "124",
+    "away_points" => "119"
   ],
-  "thirdMatch" => [
+  "3" => [
     "home_team" => "Chicago Bulls",
     "away_team" => "Philadelphia 76ers",
-    "home_points" => 100,
-    "away_points" => 95
+    "home_points" => "100",
+    "away_points" => "95"
   ],
-  "fourhMatch" => [
+  "4" => [
     "home_team" => "Trail Blazers",
     "away_team" => "Jazz",
-    "home_points" => 103,
-    "away_points" => 96
+    "home_points" => "103",
+    "away_points" => "96"
   ]
 ];
-
-for ($i=0; $i 5< ; $i++) {
-  $
-}
 ?>
 
 <!DOCTYPE html>
@@ -52,7 +48,12 @@ for ($i=0; $i 5< ; $i++) {
   <title>Document</title>
 </head>
 <body>
-
-
+  <ul>
+    <?php
+      for ($i=1; $i <= count($matches); $i++) {
+        echo "<li>".$matches[$i]["home_team"]." - ".$matches[$i]["away_team"]." | ".$matches[$i]["home_points"]." - ".$matches[$i]["away_points"]."</li>";
+      }
+    ?>
+  </ul>
 </body>
 </html>
