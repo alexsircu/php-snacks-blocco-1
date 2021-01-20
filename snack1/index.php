c014542d0668fe7054cfdd0ef432ac901e601d05
Alex Sircu <alex.sricu@gmail.com>
2021-01-20 16:56:17 +0100

    Snack1 - creato array

---
+++ snack1/index.php
@@ -0,0 +1,58 @@
+<?php
+/*
+PHP Snack 1:
+Creiamo un array 'matches' contenente altri array i
+quali rappresentano delle partite di basket di
+un’ipotetica tappa del calendario. Ogni array della
+partita avrà una squadra di casa e una squadra
+ospite, punti fatti dalla squadra di casa e punti fatti
+dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo
+schema:
+Olimpia Milano - Cantù | 55 - 60
+*/
+$matches = [
+  "firstMatch" => [
+    "home_team" => "Detroit Pistons",
+    "away_team" => "Boston Cetics",
+    "home_points" => 117,
+    "away_points" => 113
+  ],
+  "secondMatch" => [
+    "home_team" => "L.A. Lakers",
+    "away_team" => "G.S. Warriors",
+    "home_points" => 124,
+    "away_points" => 119
+  ],
+  "thirdMatch" => [
+    "home_team" => "Chicago Bulls",
+    "away_team" => "Philadelphia 76ers",
+    "home_points" => 100,
+    "away_points" => 95
+  ],
+  "fourhMatch" => [
+    "home_team" => "Trail Blazers",
+    "away_team" => "Jazz",
+    "home_points" => 103,
+    "away_points" => 96
+  ]
+];
+
+for ($i=0; $i 5< ; $i++) {
+  $
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+
+</body>
+</html>
